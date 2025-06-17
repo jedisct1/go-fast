3b2665a456a4ca940b452d819f0ee681c5773afc
Frank Denis <github@pureftpd.org>
2025-06-18 01:33:08 +0200

    Remove dead code

--- errors.go
+++ errors.go
@@ -5,12 +5,6 @@ import "errors"
 var (
 	// ErrInvalidKeySize is returned when the provided key is not 16, 24, or 32 bytes.
 	ErrInvalidKeySize = errors.New("fast: invalid key size, must be 16, 24, or 32 bytes")
-
-	// ErrNilCipher is returned when attempting to use a nil cipher instance.
-	ErrNilCipher = errors.New("fast: cipher instance is nil")
-
-	// ErrDataTooLarge is returned when the input data exceeds the maximum supported size.
-	ErrDataTooLarge = errors.New("fast: input data exceeds maximum supported size")
 )
 
 // MaxDataSize is the maximum supported data size for encryption/decryption.
